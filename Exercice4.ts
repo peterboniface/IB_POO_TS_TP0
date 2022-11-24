2040290c4c620a4f1aeb5a2b1b99d0db6fba9cc1
PeterB <peter.boniface2@gmail.com>
2022-11-24 14:37:49 +0100

    Exercice4

---
+++ Exercice4.ts
@@ -0,0 +1,15 @@
+/**
+ * function qui retourne le nombre le plus grand
+ */
+function big(num1: number, num2: number, num3: number): string {
+    if (num1 > num2 && num1 > num3){
+        return `Le plus grand nombre est ${num1}`;
+    } else if (num2 > num1 && num2 > num3){
+        return `Le plus grand nombre est ${num2}`;
+    } else if (num3 > num1 && num3 > num2) {
+        return `Le plus grand nombre est ${num3}`;
+    } else {
+        return `Les nombres doivent être différents`;
+    }
+}
+console.log(5, 10, 15);
