58637aa7ce4eddb6c4c2668998f51256fc63b2d1
PeterB <peter.boniface2@gmail.com>
2022-11-24 13:30:58 +0100

    exercice1

---
+++ Exercice1.ts
@@ -0,0 +1,8 @@
+/**
+ * fonction qui permet de dire 'Bonjour + prénom!'
+ */
+function bonjour(): string {
+    const prenom: string ="Peter";
+    return `Bonjour ${prenom}`;
+};
+console.log(bonjour());
